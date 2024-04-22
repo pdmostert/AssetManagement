179efdf0cd4bd5d84ea6675f1d3fdc7c1056f096
Peter Mostert <peter@elidz.co.za>
2024-04-22 17:38:43 +0200

    Add project files.

---
+++ Presentation/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        var serviceProvider = new ServiceCollection()
+            .AddSingleton<IConfiguration>(configuration)
+            .BuildServiceProvider();
+
+
+        //Console.WriteLine(configuration.GetValue<string>("StoragePath"));
+
+        
+
+    }
+
+
+}
+
+
